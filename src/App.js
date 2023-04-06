214317576bfa4e9b5383f25a24c1a520d59b6100
shaik-adnan1 <yousufwrk0@gmail.com>
2023-04-07 04:06:07 +0530

    final Note

--- src/App.js
+++ src/App.js
@@ -1,78 +1,130 @@
-import { Component } from "react";
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 
-import CardList from './components/card-list/card-list.component.jsx';
+import CardList from "./components/card-list/card-list.component.jsx";
 import SearchBox from "./components/search-box/search-box.component";
 
 import "./App.css";
 
-class App extends Component {
-  constructor(name) {
-    super();
+const App = () => {
+  console.log("render");
 
-    this.state = {
-      monsters: [],
-      searchField: ''
+  const [searchField, setSearchFeild] = useState(""); // [value, setValue]]
+  const [title, setTitle] = useState('')
+  const [monsters, setMonsters] = useState([]);
+  const [filterMonster, setFilterMonsters] = useState(monsters)
+
+  useEffect(() => {
+    
+      fetch("http://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(users => setMonsters(users));
+
+  }, [])
+
+
+  useEffect(() => {
+     const newFilteredMonsters = monsters.filter(monster => {
+       return monster.name.toLowerCase().includes(searchField);
+     });
+
+     setFilterMonsters(newFilteredMonsters)
+  }, [monsters, searchField])
+
+  // console.log({ searchField });
+  
+    const onSearchChange = e => {
+      const searchFieldString = e.target.value.toLowerCase();
+      setSearchFeild(searchFieldString);
     };
-    // console.log("Constructor");
-  }
+  
+    const onTitleChange = e => {
+      const setTitleString = e.target.value.toLowerCase();
+      setTitle(setTitleString);
+    };
+  
+ 
 
-  componentDidMount() {
-    // console.log("componentDidMount");
-    fetch("http://jsonplaceholder.typicode.com/users")
-      .then(response => response.json())
-      .then(users =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          },
-          // () => {
-          //   console.log(this.state);
-          // }
-        )
-      );
-  }
+  return (
+    <div className='App'>
+      <h1 className='app-title'>{title}</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder={"search monsters"}
+        className={"monsters-search-box"}
+      />
+      <br /> 
+      <SearchBox
+        onChangeHandler={onTitleChange}
+        placeholder={"set title"}
+        className={"title-search-box"}
+      />
+      <CardList monsters={filterMonster} />
+    </div>
+  );
+};
 
-  onSearchChange = e => {
-            // console.log({startingArr: this.state.monsters})
+// class App extends Component {
+//   constructor(name) {
+//     super();
 
-            const searchField = e.target.value.toLowerCase();
-            
-            this.setState(
-              () => {
-              return { searchField };
-            },
-           
+//     this.state = {
+//       monsters: [],
+//       searchField: ''
+//     };
+//     // console.log("Constructor");
+//   }
 
-            );
-          }
+//   componentDidMount() {
+//     // console.log("componentDidMount");
+//     fetch("http://jsonplaceholder.typicode.com/users")
+//       .then(response => response.json())
+//       .then(users =>
+//         this.setState(
+//           () => {
+//             return { monsters: users };
+//           },
+//           // () => {
+//           //   console.log(this.state);
+//           // }
+//         )
+//       );
+//   }
 
+//   onSearchChange = e => {
+//             // console.log({startingArr: this.state.monsters})
 
-  render() {
-    // console.log("render");
-    const {monsters, searchField} = this.state;
+//             const searchField = e.target.value.toLowerCase();
 
+//             this.setState(
+//               () => {
+//               return { searchField };
+//             },
 
-    const {onSearchChange} = this;
+//             );
+//           }
 
+//   render() {
+//     // console.log("render");
+//     const {monsters, searchField} = this.state;
 
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
-    });
+//     const {onSearchChange} = this;
 
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLowerCase().includes(searchField);
+//     });
 
-    return (
-      <div className='App'>
-        <h1 className='app-title'> MONSTERS ROLODEX</h1>
-        <SearchBox
-          onChangeHandler={onSearchChange}
-          placeholder={"search monsters"}
-          className={"monsters-search-box"}
-        />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className='App'>
+//         <h1 className='app-title'> MONSTERS ROLODEX</h1>
+//         <SearchBox
+//           onChangeHandler={onSearchChange}
+//           placeholder={"search monsters"}
+//           className={"monsters-search-box"}
+//         />
+//         <CardList monsters={filteredMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/card-component/card.component.jsx
+++ src/components/card-component/card.component.jsx
@@ -1,22 +1,17 @@
-import { Component } from "react";
+import "./card.styles.css";
 
-import './card.styles.css'
-
-
-class Card extends Component {
-    render() {
-        const [name, email, id] = this.props.monsterDetails
-        return (
-          <div className='card-container' key={id}>
-            <img
-              src={`https://robohash.org/${id}?set=set4&size=180x180`}
-              alt={`monster ${name}`}
-            />
-            <h2>{name}</h2>
-            <p>{email}</p>
-          </div>
-        );
-    }
-}
+const Card = ({ monsterDetails: [ name, email, id] }) => {
+  // const [] = props.monsterDetails;
+  return (
+    <div className='card-container' key={id}>
+      <img
+        src={`https://robohash.org/${id}?set=set2&size=180x180`}
+        alt={`monster ${name}`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
